d2b8c75028e0c665768d720fe1f23653bbb98560
DashaTsybulkina <cybulkinadara@gmail.com>
2023-03-16 17:53:04 +0300

    fixing bag

--- src/data.ts
+++ src/data.ts
@@ -4,7 +4,7 @@ import json from './folders.json'
 class Item {
     name: string
     type: string
-    parent: string
+    parent: number
     id: number
     children: Array<Item>
 
@@ -17,31 +17,16 @@ class Item {
     }
 }
 
-function parse() {
-    var jsonData: Array<Item> = []
-    if (json[0].name == "root")
-        jsonData.push(new Item(json[0].name, "folder", null, 0))
-    for (var i = 1; i < json.length; i++) {
-        if (json[i].name.indexOf("in") === -1) {
-            jsonData.push(new Item(json[i].name, json[i].name.indexOf("folder") == 0 ? "folder" : "file", "root", i));
-        } else {
-            var indexIn = json[i].name.indexOf("in")
-            console.log(indexIn)
-            jsonData.push(new Item(json[i].name.substring(0, indexIn - 1), json[i].name.indexOf("folder") < indexIn ? "folder" : "file", json[i].name.substring(indexIn + 3), i));
-        }
-    }
-    jsonData = addChildren(jsonData);
-    return jsonData
-}
-
 function addChildren(arr: Array<Item>): Array<Item> {
+    console.log(arr)
     let root: Array<Item> = []
     for (let i = 0; i < arr.length; i++) {
         if (arr[i].parent == null) {
+            console.log(arr[i])
             root.push(arr[i])
         } else
             for (let j = 0; j < arr.length; j++) {
-                if (arr[j].name == arr[i].parent) {
+                if (arr[j].id == arr[i].parent) {
                     arr[j].children.push(arr[i]);
                 }
             }
@@ -49,5 +34,4 @@ function addChildren(arr: Array<Item>): Array<Item> {
 
     return root;
 }
-
-export const data = parse()
+export const data = addChildren(json.map((obj) => new Item(obj.name, obj.type, obj.parent, obj.id)))
